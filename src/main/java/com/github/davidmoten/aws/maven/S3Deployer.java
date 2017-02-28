d3e11f9db0fea5e826e6d985fe4fb665295c8de4
Dave Moten <davidmoten@gmail.com>
2017-02-28 14:38:00 +1100

    add S3 deployer

---
+++ src/main/java/com/github/davidmoten/aws/maven/S3Deployer.java
@@ -0,0 +1,65 @@
+package com.github.davidmoten.aws.maven;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import org.apache.maven.plugin.logging.Log;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3Client;
+
+final class S3Deployer {
+
+    private final Log log;
+
+    S3Deployer(Log log) {
+        this.log = log;
+    }
+
+    public void deploy(String accessKey, String secretKey, String region, String inputDirectory,
+            final String bucketName, final String outputBasePath, Proxy proxy) {
+        final AWSCredentialsProvider credentials = new AWSStaticCredentialsProvider(
+                new BasicAWSCredentials(accessKey, secretKey));
+
+        final Region r = Region.getRegion(Regions.fromName(region));
+
+        ClientConfiguration cc = Util.createConfiguration(proxy);
+
+        final AmazonS3Client s3 = new AmazonS3Client(credentials, cc).withRegion(r);
+
+        if (inputDirectory == null) {
+            throw new RuntimeException("must specify inputDirectory parameter in configuration");
+        }
+
+        final Path root = new File(inputDirectory).toPath().toAbsolutePath();
+
+        try {
+            Files.walkFileTree(root, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                        throws IOException {
+                    String relativePath = root.relativize(file.toAbsolutePath()).toString();
+                    String objectName = outputBasePath + "/" + relativePath;
+                    log.info("uploading " + file.toFile() + " to " + bucketName + ":" + objectName);
+                    s3.putObject(bucketName, objectName, file.toFile());
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+            log.info("uploaded files");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
---
+++ src/main/java/com/github/davidmoten/aws/maven/S3DeployerMojo.java
@@ -0,0 +1,57 @@
+package com.github.davidmoten.aws.maven;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+@Mojo(name = "deployS3")
+public final class S3DeployerMojo extends AbstractMojo {
+
+    @Parameter(property = "awsAccessKey")
+    private String awsAccessKey;
+
+    @Parameter(property = "awsSecretAccessKey")
+    private String awsSecretAccessKey;
+
+    @Parameter(property = "region")
+    private String region;
+
+    @Parameter(property = "bucketName")
+    private String bucketName;
+
+    @Parameter(property = "inputDirectory")
+    private String inputDirectory;
+
+    @Parameter(property = "outputBasePath")
+    private String outputBasePath;
+
+    @Parameter(property = "httpsProxyHost")
+    private String httpsProxyHost;
+
+    @Parameter(property = "httpsProxyPort")
+    private int httpsProxyPort;
+
+    @Parameter(property = "httpsProxyUsername")
+    private String httpsProxyUsername;
+
+    @Parameter(property = "httpsProxyPassword")
+    private String httpsProxyPassword;
+
+    @Parameter(defaultValue = "${project}", required = true)
+    private MavenProject project;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+        Proxy proxy = new Proxy(httpsProxyHost, httpsProxyPort, httpsProxyUsername,
+                httpsProxyPassword);
+
+        S3Deployer deployer = new S3Deployer(getLog());
+        deployer.deploy(awsAccessKey, awsSecretAccessKey, region,  inputDirectory,bucketName,
+                outputBasePath, proxy);
+    }
+
+}
