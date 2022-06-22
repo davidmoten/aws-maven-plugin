c133d85553020e7c4531922a8c7ed7d7bb3bcd8a
Dave Moten <davidmoten@gmail.com>
2022-06-22 12:14:50 +1000

    optionally remove security groups after beanstalk deployment (because not supported properly by cloudformation)

--- src/main/java/com/github/davidmoten/aws/maven/BeanstalkDeployMojo.java
+++ src/main/java/com/github/davidmoten/aws/maven/BeanstalkDeployMojo.java
@@ -2,6 +2,10 @@ package com.github.davidmoten.aws.maven;
 
 import java.io.File;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -9,8 +13,15 @@ import org.apache.maven.project.MavenProject;
 
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
+import com.amazonaws.services.ec2.model.DescribeSecurityGroupRulesRequest;
+import com.amazonaws.services.ec2.model.Filter;
+import com.amazonaws.services.ec2.model.RevokeSecurityGroupIngressRequest;
 import com.amazonaws.services.elasticbeanstalk.AWSElasticBeanstalk;
 import com.amazonaws.services.elasticbeanstalk.AWSElasticBeanstalkClientBuilder;
+import com.amazonaws.services.elasticbeanstalk.model.DescribeEnvironmentResourcesRequest;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
@@ -38,18 +49,43 @@ public final class BeanstalkDeployMojo extends AbstractAwsMojo {
             versionLabel = createVersionLabel(applicationName, new Date(), project.getVersion());
         }
         ClientConfiguration clientConfiguration = Util.createConfiguration(proxy);
-        AWSElasticBeanstalk beanstalkClient = AWSElasticBeanstalkClientBuilder.standard()
-                .withRegion(region)
-                .withCredentials(credentials)
-                .withClientConfiguration(clientConfiguration)
-                .build();
-        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-                .withRegion(region)
-                .withCredentials(credentials)
-                .withClientConfiguration(clientConfiguration)
-                .build();
-        BeanstalkDeployer deployer = new BeanstalkDeployer(getLog(), beanstalkClient, s3Client);
+        AWSElasticBeanstalk beanstalk = AWSElasticBeanstalkClientBuilder.standard().withRegion(region)
+                .withCredentials(credentials).withClientConfiguration(clientConfiguration).build();
+        AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard().withRegion(region).withCredentials(credentials)
+                .withClientConfiguration(clientConfiguration).build();
+        AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(region).withCredentials(credentials)
+                .withClientConfiguration(clientConfiguration).build();
+        BeanstalkDeployer deployer = new BeanstalkDeployer(getLog(), beanstalk, s3);
         deployer.deploy(artifact, applicationName, environmentName, versionLabel);
+        Set<Integer> ports = new HashSet<Integer>();
+        List<String> instanceIds = beanstalk
+                .describeEnvironmentResources(
+                        new DescribeEnvironmentResourcesRequest().withEnvironmentName(environmentName)) //
+                .getEnvironmentResources() //
+                .getInstances() //
+                .stream() //
+                .map(x -> x.getId()) //
+                .collect(Collectors.toList());
+        List<String> securityGroupIds = ec2
+                .describeInstances(new DescribeInstancesRequest().withInstanceIds(instanceIds)) //
+                .getReservations() //
+                .stream() //
+                .flatMap(y -> y.getGroups().stream().map(z -> z.getGroupId())) //
+                .collect(Collectors.toList());
+
+        Filter filter = new Filter();
+        filter.setName("group-id");
+        filter.setValues(securityGroupIds);
+
+        List<String> securityGroupRuleIds = ec2
+                .describeSecurityGroupRules(new DescribeSecurityGroupRulesRequest().withFilters(filter)) //
+                .getSecurityGroupRules() //
+                .stream() //
+                .filter(x -> ports.contains(x.getToPort())) //
+                .map(x -> x.getSecurityGroupRuleId()) //
+                .collect(Collectors.toList());
+
+        ec2.revokeSecurityGroupIngress(new RevokeSecurityGroupIngressRequest().withSecurityGroupRuleIds(securityGroupRuleIds));
     }
 
     private static String createVersionLabel(String applicationName, Date date, String version) {
